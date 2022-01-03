2c3fa6e1d9f25c4cae0a146b3378a55e6115a613
Hafiz <hafizzafarahmad@gmail.com>
2022-01-03 22:25:37 +0700

    update

---
+++ app/src/main/java/com/summersoft/noteapp/feature_note/presentation/add_edit_note/NoteTextFieldState.kt
@@ -0,0 +1,7 @@
+package com.summersoft.noteapp.feature_note.presentation.add_edit_note
+
+data class NoteTextFieldState(
+    val text: String = "",
+    val hint: String = "",
+    val isVisibility: Boolean = true
+)
